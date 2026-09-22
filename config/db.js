ed807a4bc2f8e488113b3df1fe6baf9e94d1e437
webserv1 <webserv658@gmail.com>
2026-09-22 17:46:38 +0530

    feat: Update database configuration to prioritize DATABASE_URL for connection

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const { Pool } = require("pg");
+
 require("dotenv").config();
 
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL must be set");
+}
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString,
 });
 
 module.exports = pool;
